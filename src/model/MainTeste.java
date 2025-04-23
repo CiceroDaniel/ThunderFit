91cad645f8c46a81e5c3d30a421ba18a9e513abc
VictorHugoDD <vic.hdd2006@gmail.com>
2025-04-23 12:20:06 -0300

    Validaçao de dados no construtor do usuario

--- src/model/MainTeste.java
+++ src/model/MainTeste.java
@@ -7,12 +7,9 @@ import repository.UsuarioRepository;
 public class MainTeste {
 
 	public static void main(String[] args) {
-		Usuario a1=new Aluno("Victr","111.111.111-22","VIC.HDD", 1.75, 54, Metas.perderPeso,"Perder 8kg", Plano.planoAnual);
-		UsuarioRepository u=new UsuarioRepository();
-		u.cadastro(a1);
-		System.out.println(u.listarTodos());
-		a1.setCpf("222.333");
-		u.atualizarDados(a1);
+		Usuario a1=new Aluno("Victr","a123456789101112","VIC.HDD", 1.75, 54, Metas.perderPeso,"Perder 8kg", Plano.planoAnual);
+		
+		System.out.println(a1.gerarCredenciais());
 		
 		
 	}
--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -8,6 +8,7 @@ public abstract class Usuario{
 		protected String nome;
 		protected String cpf;
 		protected String email;
+		
 		//protected LocalDate dataDeNascimento;
 		//protected Endereco endereco;
 		protected boolean status;
@@ -16,9 +17,9 @@ public abstract class Usuario{
 		
 		//**--------------CONSTRUTOR----------------------**//
 		public Usuario(String nome, String cpf, String email) {
-			this.nome = nome;
-			this.cpf = cpf;
-			this.email = email;
+			this.setNome(nome);
+			this.setCpf(cpf);
+			this.setEmail(email);
 			//this.dataDeCadastro = LocalDate.now();
 			this.status = true;
 		}

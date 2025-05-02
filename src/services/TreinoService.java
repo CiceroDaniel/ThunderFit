f99807e72fd7343b78e08cbfb115f87ab29d36dc
VictorHugoDD <vic.hdd2006@gmail.com>
2025-05-02 17:04:38 -0300

    add: TreinoService e ExercicioService

---
+++ src/services/TreinoService.java
@@ -0,0 +1,11 @@
+package services;
+
+import model.*;
+import repository.TreinoRepository;
+import repository.ExercicioRepository;
+import repository.UsuarioRepository;;
+
+
+public class TreinoService {
+
+}

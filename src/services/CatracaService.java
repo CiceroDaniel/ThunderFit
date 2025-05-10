a84b04e4bfddeb6634c123d93785790391b2534f
VictorHugoDD <vic.hdd2006@gmail.com>
2025-05-10 14:00:07 -0300

    add: catracaService

---
+++ src/services/CatracaService.java
@@ -0,0 +1,50 @@
+package services;
+
+import model.Catraca;
+import model.Pagamento;
+import repository.PagamentoRepository;
+
+import java.util.List;
+import java.util.Objects;
+
+import model.Aluno;
+
+public class CatracaService{
+	private final Catraca catraca;
+	private final PagamentoRepository pagamentoRepository;
+	private final PresencaService presencaService;
+	
+	public CatracaService(Catraca catraca, PagamentoRepository pagamentoRepository, PresencaService presencaService){
+		this.catraca=catraca;
+		this.pagamentoRepository= pagamentoRepository;
+		this.presencaService=presencaService;
+	}
+	
+	public boolean liberarAcesso(Aluno aluno) {
+		if(aluno == null) return false;
+		List<Pagamento> pagamentosDoAluno = pagamentoRepository.listarPorAluno(aluno.getCpf());
+		
+		if(pagamentosDoAluno.isEmpty())	return false;
+		
+		Pagamento ultimoPagamento = pagamentosDoAluno.stream()
+				.max((p1,p2)-> p1.getDataPagamento().compareTo(p2.getDataPagamento())).orElse(null);
+		
+		if(ultimoPagamento == null || ultimoPagamento.estaVencido()) return false;
+		
+		
+	  catraca.liberar(aluno);
+	  return true;
+		
+	}
+	
+	public boolean registrarPassagem(Aluno aluno) {
+		if(!liberarAcesso(aluno)){
+			return false;
+		}
+		presencaService.registrarPresenca(aluno);
+		catraca.bloquear();
+		return true;
+		
+	}
+	
+}

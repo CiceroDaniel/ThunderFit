4708b0efb56601b91b1f02875e4d140c47bc03c7
VictorHugoDD <vic.hdd2006@gmail.com>
2025-05-11 00:54:41 -0300

    add: exercicios iniciados na main console

--- src/app/teste.java
+++ src/app/teste.java
@@ -3,15 +3,17 @@ package app;
 import  java.util.Scanner;	
 import app.consoleMenu;
 import app.toolbox;
+import repository.ExercicioRepository;
 import repository.UsuarioRepository;
 import services.AuthService;
+import services.ExercicioService;
 import services.UsuarioService;
 public class teste {
 	
 	
 	public static void main(String[] args) {
-		
-		
+		final ExercicioRepository exRepo = new ExercicioRepository();
+		final ExercicioService exServi = new ExercicioService(exRepo);
 		final UsuarioRepository repo = new UsuarioRepository();
 		final UsuarioService services = new UsuarioService(repo);
 		final AuthService auth = new AuthService(repo,services);
